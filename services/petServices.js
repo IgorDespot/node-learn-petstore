681fa2d3f7a4b3ac302f77f902775e22f75230ca
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-16 17:18:42 +0200

    change function for find pets by status

--- services/petServices.js
+++ services/petServices.js
@@ -1,5 +1,5 @@
 const Pet = require('../models/Pet');
-//const logger = require('../../util/logger');
+const logger = require('../util/logger');
 
 const createPet = (req, res, next) => {
 
@@ -20,18 +20,32 @@ const createPet = (req, res, next) => {
 const getPetsByStatus = (req, res, next) => {
     logger.info(`GET fired: get pets by status. ${Date(Date.now())}`);
 
-    Pet.find({
-        status: req.params.status
-    }, (err, pets) => {
-        if (err) return res.status(500).send(err);
-        else if (status === 'AVAILABLE') {
-            return res.status(200).send(pets)
-        } else if (status === 'PENDING') {
-            return res.status(200).send(pets)
-        } else if (status === 'SOLD') {
-            return res.status(200).send(pets)
-        } else return res.status(404).send('No pets with such status found');
-    });
+    let status = req.params.status;
+
+    if (status == 'available') {
+        Pet.find({
+            "status": "AVAILABLE"
+        }, (err, pets) => {
+            if (err) return res.status(500).send("There was a problem finding the pets.");
+            res.status(200).send(pets);
+        });
+    } else if (status == 'pending') {
+        Pet.find({
+            "status": "PENDING"
+        }, (err, pets) => {
+            if (err) return res.status(500).send("There was a problem finding the pets.");
+            res.status(200).send(pets);
+        });
+    } else if (status == 'sold') {
+        Pet.find({
+            "status": "SOLD"
+        }, (err, pets) => {
+            if (err) return res.status(500).send("There was a problem finding the pets.");
+            res.status(200).send(pets);
+        });
+    } else {
+        return res.status(404).send("No pets with such status found... Available statuses: 'available', 'pending' and 'sold' ");
+    }
 };
 
 
