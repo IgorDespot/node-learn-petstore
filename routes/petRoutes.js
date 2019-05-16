53ea65a1d253bd136722fb6371fd066b3036586f
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-16 16:07:03 +0200

    create route for find pets by status

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router();
-const { createPet, getPetById } = require('../services/petServices');
+const {
+    createPet,
+    getPetById,
+    getPetsByStatus
+} = require('../services/petServices');
+
 
 
 router.post('/', createPet);
 router.get('/:id', getPetById);
+router.get('/getPetsByStatus/:status', getPetsByStatus);
+
+
 
 module.exports = router;
