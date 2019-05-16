d2ed874d2be082095b312c6122f0dc13c9ff6085
StefanAtanaskovic <stefan.atanaskovi@eng.it>
2019-05-16 14:08:43 +0200

    Fixed spelling in error msg

--- models/User.js
+++ models/User.js
@@ -14,39 +14,39 @@ const validateEmail = (email) => {
 let UserSchema = new Schema({
     username: {
         value: String,
-        required: [true, 'This filled must be filled'],
+        required: [true, 'This field must be filled'],
         trim: true
     },
     firstName: {
         value: String,
-        required: [true, 'This filled must be filled'],
+        required: [true, 'This field must be filled'],
         trim: true
     },
     lastName: {
         value: String,
-        required: [true, 'This filled must be filled'],
+        required: [true, 'This field must be filled'],
         trim: true
     },
     email: {
         value: String,
-        required: [true, 'This filled must be filled'],
+        required: [true, 'This field must be filled'],
         trim: true,
         validation: [validateEmail, 'You must enter a valid email']
     },
     password: {
         value: String,
-        required: [true, 'This filled must be filled'],
+        required: [true, 'This field must be filled'],
         trim: true,
         validate: [validatePass, 'You password must be atleast 8 characters long']
     },
     phone: {
         value: String,
         trim: true,
-        required: [true, 'This filled must be filled']
+        required: [true, 'This field must be filled']
     },
     userStatus: {
         value: Number,
-        required: [true, 'This filled must be filled'],
+        required: [true, 'This field must be filled'],
         trim: true
     }
 });
