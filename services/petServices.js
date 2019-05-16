fdc7a3c697e23899ee9bfabcd466032e55762111
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-16 15:57:58 +0200

    create get method: find pets by status

--- services/petServices.js
+++ services/petServices.js
@@ -1,19 +1,41 @@
 const Pet = require('../models/Pet');
 //const logger = require('../../util/logger');
 
-const createPet = (req, res,next) => {
+const createPet = (req, res, next) => {
 
     newPet = new Pet(req.body);
 
     newPet.save().then(pet => {
-       //logger.info( 'New pet is saved.');
-       res.status(200).send(pet);
+        //logger.info( 'New pet is saved.');
+        res.status(200).send(pet);
     }).catch((err) => {
-       res.status(405).send('Invalid input');
-       //logger.error('Invalid input');
+        res.status(405).send('Invalid input');
+        //logger.error('Invalid input');
     })
-}
+};
+
+
+// GET PETS BY STATUS
+// **
+const getPetsByStatus = (req, res, next) => {
+    logger.info(`GET fired: get pets by status. ${Date(Date.now())}`);
+
+    Pet.find({
+        status: req.params.status
+    }, (err, pets) => {
+        if (err) return res.status(500).send(err);
+        else if (status === 'AVAILABLE') {
+            return res.status(200).send(pets)
+        } else if (status === 'PENDING') {
+            return res.status(200).send(pets)
+        } else if (status === 'SOLD') {
+            return res.status(200).send(pets)
+        } else return res.status(404).send('No pets with such status found');
+    });
+};
+
 
 module.exports = {
-    createPet
+    createPet,
+    getPetsByStatus
 }
