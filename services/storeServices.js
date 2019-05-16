d89764829952c0d772509d21bca676a6b8ee3e6a
Aleksandar Stojmenovic <aleksandar.stojmenovic9@gmail.com>
2019-05-16 16:39:46 +0200

    Add functions for getting Order by ID

--- services/storeServices.js
+++ services/storeServices.js
@@ -9,8 +9,24 @@ var createOrder = (req, res, next) => {
             res.status(404).send("Cannot create Order");
         });
 }
+var findByOrderId = (req, res, next) => {
+    let id = req.params.id;
+    Order.find({ _id: id })
+        .then(function (order) {
 
+            if (order) {
+                res.status(200).send(order);
+            } else {
+                res.status(404).send("Cannot find Order");
+            }
+
+        }).catch(function (err) {
+            console.log(err);
+            res.status(400).send("ID is wrong");
+        });
+}
 
 module.exports = {
-    createOrder
+    createOrder,
+    findByOrderId
 }
